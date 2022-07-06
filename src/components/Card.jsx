d0ce5431ab109c047515abca8e4ad889b051b049
Luis Medina <lamc3005@gmail.com>
2022-07-06 18:12:15 -0400

    factor functions and create request in external file

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,35 @@
 import React, { useState } from "react";
-import axios from "axios";
 import divider from "../images/pattern-divider-desktop.svg";
 import dice from "../images/icon-dice.svg";
+import getAdvice from "../services/getAdvice";
+import spinner from "../images/Spinner-1s-200px.svg";
+
 const Card = () => {
   const [text, setText] = useState({
     id: 177,
     advice:
       "It is easy to sit up an take notice, what's difficult is getting up and taking action.",
   });
+  const [loading, setLoading] = useState(false);
 
   function obtainNewAdivice() {
-    axios
-      .get("https://api.adviceslip.com/advice")
-      .then((response) => {
-        setText(response.data.slip);
-      })
-      .catch((error) => {
-        console.log(`Something went wrong: ${error}`);
-      })
-      .finally(() => {
-        console.log("Done");
-      });
+    setLoading(true);
+    getAdvice().then((e) => {
+      setText(e);
+      setTimeout(() => setLoading(false), 1000)
+    })
   }
 
   return (
     <div className="container">
-      <p className="text-id">Advice #{text.id}</p>
-      <p className="text-advice">"{text.advice}"</p>
+      {loading ? (
+        <img src={spinner} alt="spinner" />
+      ) : (
+        <div>
+          <p className="text-id">Advice #{text.id}</p>
+          <p className="text-advice">"{text.advice}"</p>
+        </div>
+      )}
       <img className="img divider" src={divider} alt="divisor" />
       <div className="container-img">
         <img className="img" onClick={obtainNewAdivice} src={dice} alt="dice" />
